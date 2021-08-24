5aaeb037efe806d3ee215cc78a1b38fd1fb1f3d5
DevAndyroo <andy@andyroo.info>
2021-08-24 12:26:22 +0100

    removed unused extend

--- test/pages/behaviours/PageBehaviours.scala
+++ test/pages/behaviours/PageBehaviours.scala
@@ -17,7 +17,6 @@
 package pages.behaviours
 
 import base.SpecBase
-import generators.Generators
 import models.UserAnswers
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.{Arbitrary, Gen}
@@ -25,7 +24,7 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import pages.QuestionPage
 import play.api.libs.json._
 
-trait PageBehaviours extends SpecBase with ScalaCheckPropertyChecks with Generators {
+trait PageBehaviours extends SpecBase with ScalaCheckPropertyChecks {
 
   class BeRetrievable[A] {
     def apply[P <: QuestionPage[A]](genP: Gen[P])(implicit ev1: Arbitrary[A], ev2: Format[A]): Unit = {
