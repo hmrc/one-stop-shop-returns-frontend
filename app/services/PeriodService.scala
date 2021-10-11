e42f165a7a9eb04fc2799db7ea27d6e95d1a2eef
DevAndyroo <andy@andyroo.info>
2021-10-11 17:06:21 +0100

    VEOSS-515 initial handling of multiple periods

---
+++ app/services/PeriodService.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import models.{Period, Quarter}
+import models.Quarter._
+
+import javax.inject.Inject
+import java.time.{Clock, LocalDate}
+import scala.:+
+
+class PeriodService @Inject()(clock: Clock) {
+
+  def getAvailablePeriods(commencementDate: LocalDate): Seq[Period] = {
+//    val today = LocalDate.now(clock)
+    val year = 2021
+    val period = Period(year, Q3)
+
+    if(period.lastDay.plusDays(1).isAfter(commencementDate)) {
+      Seq(period)
+    } else {
+      Seq.empty
+    }
+  }
+
+  def getAllPeriods(): Seq[Period] = {
+    val firstPeriod = Period(2021, Q3)
+    getPeriodsUntilDate(firstPeriod, LocalDate.now(clock))
+  }
+
+  private def getPeriodsUntilDate(currentPeriod: Period, endDate: LocalDate): Seq[Period] = {
+    if(currentPeriod.lastDay.isBefore(endDate)) {
+      Seq(currentPeriod) ++ getPeriodsUntilDate(getNextPeriod(currentPeriod), endDate)
+    } else {
+      Seq.empty
+    }
+  }
+
+  def getNextPeriod(currentPeriod: Period): Period = {
+    currentPeriod.quarter match {
+      case Q4 =>
+        Period(currentPeriod.year + 1, Q1)
+      case Q3 =>
+        Period(currentPeriod.year, Q4)
+      case Q2 =>
+        Period(currentPeriod.year, Q3)
+      case Q1 =>
+        Period(currentPeriod.year, Q2)
+    }
+  }
+}
