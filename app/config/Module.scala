d43ee45732c9686a002013dc9a57e608dda1170b
owengilfellon <owen.gilfellon@opencastsoftware.com>
2021-12-15 11:16:51 +0000

    VEOSS-713 - More Save and Continue text updates

--- app/config/Module.scala
+++ app/config/Module.scala
@@ -19,13 +19,20 @@ package config
 import com.google.inject.AbstractModule
 import controllers.actions._
 
-import java.time.{Clock, Instant, ZoneId, ZoneOffset}
+import java.time.{Clock, Instant, ZoneId, ZoneOffset, ZonedDateTime}
 
 class Module extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[DataRequiredAction]).to(classOf[DataRequiredActionImpl]).asEagerSingleton()
-    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone.withZone(ZoneOffset.UTC))
+    bind(classOf[Clock]).toInstance(
+      Clock.fixed(
+        ZonedDateTime.of(
+          2022, 12, 1, 1, 0, 0, 0, ZoneOffset.UTC
+        ).toInstant,
+        ZoneId.of("Australia/Melbourne")
+      )
+    )
     bind(classOf[AuthenticatedControllerComponents]).to(classOf[DefaultAuthenticatedControllerComponents]).asEagerSingleton()
   }
 }
