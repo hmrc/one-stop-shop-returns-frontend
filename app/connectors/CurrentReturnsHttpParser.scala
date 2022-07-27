68268b16c0a0cdece12ed53dc440ca2fde016bb4
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2022-07-27 11:50:08 +0100

    VEOSS-1157 | Add exclude user flag. Update mongodb version

--- app/connectors/CurrentReturnsHttpParser.scala
+++ app/connectors/CurrentReturnsHttpParser.scala
@@ -21,17 +21,17 @@ import models.responses._
 import play.api.http.Status._
 import play.api.libs.json.{JsError, JsSuccess}
 import uk.gov.hmrc.http.{HttpReads, HttpResponse}
-import viewmodels.yourAccount.Return
+import viewmodels.yourAccount.{CurrentReturns, Return}
 
 object CurrentReturnsHttpParser extends Logging {
 
-  type CurrentReturnsResponse = Either[ErrorResponse, Seq[Return]]
+  type CurrentReturnsResponse = Either[ErrorResponse, CurrentReturns]
 
   implicit object CurrentReturnsReads extends HttpReads[CurrentReturnsResponse] {
     override def read(method: String, url: String, response: HttpResponse): CurrentReturnsResponse = {
       response.status match {
         case OK | CREATED =>
-          response.json.validate[Seq[Return]] match {
+          response.json.validate[CurrentReturns] match {
             case JsSuccess(returnsModel, _) => Right(returnsModel)
             case JsError(errors) =>
               logger.warn(s"Failed trying to parse JSON $errors. Json was ${response.json}", errors)
