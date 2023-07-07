2fcd3bb31ef0c8fd4defd02da4f2a9fc2373d3cc
StefanTagg <stefanctagg@gmail.com>
2023-07-07 14:41:34 +0100

    VEOSS-1671 Updated hmrc-frontend version to fix increase width of underline for hover state

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,9 +5,9 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.1.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.15.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.14.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.19.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
     "uk.gov.hmrc"       %% "domain"                         % "8.1.0-play-28",
     "org.typelevel"     %% "cats-core"                      % "2.7.0"
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"               % "3.2.12",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % "7.15.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % "7.19.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
     "org.pegdown"             %  "pegdown"                 % "1.6.0",
     "org.jsoup"               %  "jsoup"                   % "1.14.3",
