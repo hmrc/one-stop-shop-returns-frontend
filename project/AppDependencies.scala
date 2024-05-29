2a6bc2db9d610d027c18c87bf483d84a885d49ea
Vincent Munier <vincent.munier@digital.hmrc.gov.uk>
2024-05-29 14:21:30 +0100

    Upgrade scala play and scoverage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,10 +3,13 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
+  private val httpVerbsVersion = "15.0.0"
+
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "8.5.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "http-verbs-play-30"                     % httpVerbsVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % "8.4.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "1.7.0",
     "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0",
