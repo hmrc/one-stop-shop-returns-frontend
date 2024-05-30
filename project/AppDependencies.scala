defee4dda524c072dfca47c037076bb8a4ebd035
Vincent Munier <vincent.munier@digital.hmrc.gov.uk>
2024-05-30 15:20:06 +0100

    Fix module

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,6 +3,7 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
+  private val bootstrapVersion = "8.4.0"
   private val httpVerbsVersion = "15.0.0"
 
   val compile = Seq(
@@ -10,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "8.5.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
     "uk.gov.hmrc"       %% "http-verbs-play-30"                     % httpVerbsVersion,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % "8.4.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "1.7.0",
     "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0",
     "org.typelevel"     %% "cats-core"                              % "2.9.0"
@@ -20,7 +21,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"               % "3.2.15",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-4-6"             % "3.2.15.0",
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % "8.4.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
     "org.pegdown"             %  "pegdown"                 % "1.6.0",
     "org.jsoup"               %  "jsoup"                   % "1.15.4",
