d2a9e33e2bb8cc61d110514721cfdf483123265a
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-07-01 15:01:43 +0100

    VEOSS-2106: Add frontend configuration to enable the rebrand.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.13.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.13.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.6.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
