7ebd1049cd348164c2b536b692d0d805c516b428
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2025-10-13 12:20:03 +0100

    VEOSS-2138 update dependencies the right way

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ object AppDependencies {
   import play.core.PlayVersion
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcMongoVersion = "2.9.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.17.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % "2.9.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0",
     "org.typelevel"     %% "cats-core"                              % "2.13.0"
   )
@@ -24,7 +24,7 @@ object AppDependencies {
     "org.jsoup"               %  "jsoup"                   % "1.21.1",
     "org.playframework"       %% "play-test"               % PlayVersion.current,
     "org.scalacheck"          %% "scalacheck"              % "1.18.1",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % "2.9.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.64.8"
   ).map(_ % "test, it")
 
