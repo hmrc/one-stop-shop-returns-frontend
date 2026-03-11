438bbb762373eb2da57c93d67616f399b452215b
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2026-03-11 10:27:45 +0000

    VEI-815 | Update libraries to fix reported vulnerabilities

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -20,7 +20,6 @@ import com.google.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
-import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
 
 import java.net.URI
 
@@ -35,7 +34,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   private val contactFormServiceIdentifier = "one-stop-shop-returns-frontend"
 
   def feedbackUrl(implicit request: RequestHeader): String =
-    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
+    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${host + request.uri}"
 
   val loginUrl: String         = configuration.get[String]("urls.login")
   val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.31.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.32.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                         % "13.0.0",
