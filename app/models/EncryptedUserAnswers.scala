d61c10459c66f614f57041e2e7f20bb394319a44
Andrew Smith <andy@andyroo.info>
2026-04-29 17:01:17 +0100

    Encryption key

--- app/models/EncryptedUserAnswers.scala
+++ app/models/EncryptedUserAnswers.scala
@@ -17,11 +17,9 @@
 package models
 
 import play.api.libs.json.*
-import queries.{Derivable, Gettable, Settable}
 import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 
 import java.time.Instant
-import scala.util.{Failure, Success, Try}
 
 case class EncryptedUserAnswers(
                               userId: String,
